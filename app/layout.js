03af86ac30c1f9dbeb441131acec530aa0273d15
Rares-Ubuntu <rares.badici@upb.ro>
2026-04-07 15:58:43 +0300

    facut non indexabil

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,16 @@ import "./globals.css";
 
 export const metadata = {
   title: "Serviciul Online | Platforme digitale UNSTPB",
-  description: "Lista platformelor digitale administrate de biroul Serviciul Online din cadrul UNSTPB."
+  description: "Lista platformelor digitale administrate de biroul Serviciul Online din cadrul UNSTPB.",
+  robots: {
+    index: false,
+    follow: false,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true
+    }
+  }
 };
 
 export default function RootLayout({ children }) {
